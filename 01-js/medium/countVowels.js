913fce9c27bad3a8a0c6365b5b5eb6455be76c0d
karanrao-gitinit <karanrao81441@outlook.com>
2023-12-10 19:17:16 +0530

    Added solution on countVowels.js file

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,24 @@
 */
 
 function countVowels(str) {
-    // Your code here
+/**
+ * STEP 1
+ * -------
+ * Used Match method to match the 'a, e, i, o, u' in the parameter
+ * Used litral Regular expression and 'gi' is 'g = Global' and 'i = case-insensetive' *
+ */
+    const matchVowels = str.match(/[aeiou]/gi);
+    /**
+     * STEP 2
+     * ------
+     * Used ternary operator to define the condition
+     * Condition: if vowels matched then it should return the length of matchVowels
+     * which stores the matched vowels else it will return 0
+     */  
+    return matchVowels ? matchVowels.length : 0;
 }
+
+const str = 'I am a hero';
+console.log(countVowels(str));
 
 module.exports = countVowels;
