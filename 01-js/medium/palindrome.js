a4bbb171e0a6078e3b6e14551bb2ddf77806349f
karanrao-gitinit <karanrao81441@outlook.com>
2023-12-10 20:04:43 +0530

    Added solution on palindrome.js file

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,7 +4,37 @@
 */
 
 function isPalindrome(str) {
-  return true;
+
+  /**
+   * STEP 1
+   * ------
+   * Created new Constant called 'cleanStr' to store cleaned String
+   * Convert string to lower case and remove non-alphanumeric characters
+   * used regular expression to replace non-alphanumeric characters
+   */
+  const cleanStr = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+
+  /**
+   * STEP 2
+   * ------
+   * Splitted the String into single characters
+   * Reversed and joined it to create a new string
+   * Stored in as a new constant called 'reversedStr'  
+   */
+  const reversedStr = cleanStr.split('').reverse().join('');
+  
+  /**
+   * STEP 3
+   * ------
+   * Used ternary operator to define the condition
+   * Condition: if values matched it should return true else false   
+   */
+
+  return cleanStr === reversedStr ? true : false;
 }
+
+
+const str = 'Able, was I ere I saw Elba!'
+console.log(isPalindrome(str));
 
 module.exports = isPalindrome;
